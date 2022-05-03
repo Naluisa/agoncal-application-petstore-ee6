da191416a08f73dbb21a3837265bcc352335f5e3
Naluisa <anaalutorres@gmail.com>
2022-05-03 17:22:48 -0300

    Adicionando a ferramenta @Getter/@Setter do Lombok na classe Product.java para implementar automaticamente os métodos Getter/Setters da classe

--- src/main/java/org/agoncal/application/petstore/domain/Product.java
+++ src/main/java/org/agoncal/application/petstore/domain/Product.java
@@ -7,6 +7,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -28,6 +30,8 @@ public class Product {
     // =             Attributes             =
     // ======================================
 
+    @Setter
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -66,52 +70,7 @@ public class Product {
         this.category = category;
     }
 
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public void addItem(Item item) {
-        if (items == null)
-            items = new ArrayList<Item>();
-        items.add(item);
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
-
-    public void setItems(List<Item> items) {
-        this.items = items;
-    }
-
+    
     // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
