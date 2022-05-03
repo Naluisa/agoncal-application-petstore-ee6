6ff9f40443e3808eba0a01d2ab7c2cb69cafbc37
Naluisa <anaalutorres@gmail.com>
2022-05-03 17:01:37 -0300

    Adicionando a ferramenta @Getter/@Setter do Lombok na classe Order.java para implementar automaticamente os métodos Getter/Setters da classe

--- src/main/java/org/agoncal/application/petstore/domain/Order.java
+++ src/main/java/org/agoncal/application/petstore/domain/Order.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Antonio Goncalves
@@ -23,6 +25,8 @@ public class Order {
     // =             Attributes             =
     // ======================================
 
+    @Setter
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -86,74 +90,6 @@ public class Order {
         }
 
         return total;
-    }
-
-    // ======================================
-    // =         Getters & setters          =
-    // ======================================
-
-    public Long getId() {
-        return id;
-    }
-
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
-    }
-
-    public List<OrderLine> getOrderLines() {
-        return orderLines;
-    }
-
-    public void setOrderLines(List<OrderLine> orderLines) {
-        this.orderLines = orderLines;
-    }
-
-    public Address getDeliveryAddress() {
-        return deliveryAddress;
-    }
-
-    public void setDeliveryAddress(Address deliveryAddress) {
-        this.deliveryAddress = deliveryAddress;
-    }
-
-    public CreditCard getCreditCard() {
-        return creditCard;
-    }
-
-    public void setCreditCard(CreditCard creditCard) {
-        this.creditCard = creditCard;
-    }
-
-    public String getCreditCardNumber() {
-        return creditCard.getCreditCardNumber();
-    }
-
-    public void setCreditCardNumber(String creditCardNumber) {
-        creditCard.setCreditCardNumber(creditCardNumber);
-    }
-
-    public CreditCardType getCreditCardType() {
-        return creditCard.getCreditCardType();
-    }
-
-    public void setCreditCardType(CreditCardType creditCardType) {
-        creditCard.setCreditCardType(creditCardType);
-    }
-
-    public String getCreditCardExpiryDate() {
-        return creditCard.getCreditCardExpDate();
-    }
-
-    public void setCreditCardExpiryDate(String creditCardExpiryDate) {
-        creditCard.setCreditCardExpDate(creditCardExpiryDate);
     }
 
     // ======================================
